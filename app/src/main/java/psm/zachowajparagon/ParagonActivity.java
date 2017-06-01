38e4a6bbf7b9691715e461bc1743c7ee85ce72d3
Olsz4k <olszanskimateusz@gmail.com>
2017-06-01 17:32:40 +0200

    Dodanie robienia zdjęcia z kamery telefonu + podłączenie google places api do wybierania sklepu.

--- app/src/main/java/psm/zachowajparagon/ParagonActivity.java
+++ app/src/main/java/psm/zachowajparagon/ParagonActivity.java
@@ -2,16 +2,28 @@ package psm.zachowajparagon;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -25,17 +37,33 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.R.attr.path;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class ParagonActivity extends AppCompatActivity {
 
-    private ImageButton mSelectImage;
+    private Button mSelectImage;
     private EditText mPostTitle;
     private EditText mPostDesc;
+    private EditText mPostShop;
+    private Spinner mPostCategory;
+    private EditText mPostPrice;
     private Button mSubmitBtn;
     private Uri mImageUri = null;
-
-    private static final int GALLERY_REQUEST = 1;
-
+    private Uri resultUri;
+    private Uri fileUri = null;
+    private String mShopValPicker = "";
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private StorageReference mStorage;
     private DatabaseReference mDatabase;
     private ProgressDialog mProgress;
@@ -49,6 +77,8 @@ public class ParagonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paragon);
 
+
+
         mAuth = FirebaseAuth.getInstance();
 
         mCurrentUser = mAuth.getCurrentUser();
@@ -60,10 +90,15 @@ public class ParagonActivity extends AppCompatActivity {
         mDatabaseUser = FirebaseDatabase.getInstance().getReference().child(mCurrentUser.getUid());
 
 
-        mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
+        mSelectImage = (Button) findViewById(R.id.imageSelect);
 
         mPostTitle = (EditText) findViewById(R.id.titleField);
+       // mPostShop = (EditText) findViewById(R.id.shopField);
+        mPostCategory = (Spinner) findViewById(R.id.categoryField);
+        mPostPrice = (EditText) findViewById(R.id.priceField);
         mPostDesc = (EditText) findViewById(R.id.descField);
+
+
         mSubmitBtn = (Button) findViewById(R.id.submitBtn);
 
         mProgress = new ProgressDialog(this);
@@ -71,40 +106,97 @@ public class ParagonActivity extends AppCompatActivity {
 
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            public void onClick(View v) {
+                final Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                fileUri=  getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 
             }
         });
+
+
+
 
         mSubmitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                Toast.makeText(ParagonActivity.this, "A"+fileUri, Toast.LENGTH_LONG).show();
+
+
                 startParagon();
 
             }
         });
+
+
+
+        PlaceAutocompleteFragment places= (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        places.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                mShopValPicker = ""+place.getName();
+            }
+
+            @Override
+            public void onError(Status status) {
+
+                Toast.makeText(getApplicationContext(),status.toString(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+    }
+
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    private static File getOutputMediaFile(int type){
+
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory(), "MyCameraApp");
+
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
     }
 
     private void startParagon() {
 
-        mProgress.setMessage("Posting...");
+        mProgress.setMessage("Trwa zapisywanie paragonu...");
 
 
         final String title_val = mPostTitle.getText().toString().trim();
         final String desc_val = mPostDesc.getText().toString().trim();
+        final String shop_val = mShopValPicker;
+        final String price_val = mPostPrice.getText().toString().trim();
 
-        if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null) {
+
+        if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && resultUri != null) {
 
             mProgress.show();
+            StorageReference filepath = mStorage.child("Receipts_images").child(resultUri.getLastPathSegment());
 
-            StorageReference filepath = mStorage.child("Receipts_images").child(mImageUri.getLastPathSegment());
-
-            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            filepath.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
@@ -119,6 +211,8 @@ public class ParagonActivity extends AppCompatActivity {
 
                             newReceipt.child("title").setValue(title_val);
                             newReceipt.child("desc").setValue(desc_val);
+                            newReceipt.child("shop").setValue(shop_val);
+                            newReceipt.child("price").setValue(price_val);
                             newReceipt.child("image").setValue(downloadUrl.toString());
                             newReceipt.child("uid").setValue(mCurrentUser.getUid());
                             newReceipt.child("username").setValue(dataSnapshot.child("name").getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -141,13 +235,8 @@ public class ParagonActivity extends AppCompatActivity {
                         }
                     });
 
-
                     mProgress.dismiss();
-
                     startActivity(new Intent(ParagonActivity.this, MainActivity.class));
-
-
-
                 }
             });
 
@@ -158,13 +247,56 @@ public class ParagonActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            CropImage.activity(fileUri)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                resultUri = result.getUri();
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+}
+
+
+
+/*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
 
             mImageUri = data.getData();
 
-            mSelectImage.setImageURI(mImageUri);
+            CropImage.activity(mImageUri)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+
+
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+
+               // mSelectImage.setImageURI(resultUri);
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
         }
     }
-}
+    */
+
--- app/src/main/java/psm/zachowajparagon/Receipt.java
+++ app/src/main/java/psm/zachowajparagon/Receipt.java
@@ -9,14 +9,23 @@ public class Receipt {
     private String title;
     private String desc;
     private String image;
+    private int price;
+    private String place;
+    private String category;
+    private int guarantee;
+
 
     public Receipt() {
     }
 
-    public Receipt(String title, String desc, String image) {
+    public Receipt(String title, String desc, String image, int price, String place, String category, int guarantee) {
         this.title = title;
         this.desc = desc;
         this.image = image;
+        this.price = price;
+        this.place = place;
+        this.category = category;
+        this.guarantee = guarantee;
     }
 
     public String getTitle() {
@@ -41,5 +50,21 @@ public class Receipt {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setGuarantee(int guarantee) {
+        this.guarantee = guarantee;
     }
 }
